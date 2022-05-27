fc763f6213122867fee0bf35652f31333f911a5f
Marck <Marck527@gmail.com>
2022-05-27 15:42:44 +1000

    Added logout route and handler

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -12,5 +12,6 @@ func SetRoutes(r *mux.Router) {
 	r.HandleFunc("/ping", controllers.PingHandler).Methods("GET")
 	r.HandleFunc("/register", controllers.RegisterHandler).Methods("POST")
 	r.HandleFunc("/authenticate", controllers.AuthHandler).Methods("POST")
+	r.HandleFunc("/logout", controllers.LogoutHandler).Methods("POST")
 	r.HandleFunc("/search", middlewares.ValidateJWT(controllers.SearchHandler)).Methods("POST")
 }
