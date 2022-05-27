66f341eb2e174a70a96dce402d981453ecf6f2b5
Marck <Marck527@gmail.com>
2022-05-27 15:36:33 +1000

    Wrapped search endpoint with auth middleware

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	controllers "backend/controllers"
+	"backend/middlewares"
 
 	"github.com/gorilla/mux"
 )
@@ -11,5 +12,5 @@ func SetRoutes(r *mux.Router) {
 	r.HandleFunc("/ping", controllers.PingHandler).Methods("GET")
 	r.HandleFunc("/register", controllers.RegisterHandler).Methods("POST")
 	r.HandleFunc("/authenticate", controllers.AuthHandler).Methods("POST")
-	r.HandleFunc("/search", controllers.SearchHandler).Methods("POST")
+	r.HandleFunc("/search", middlewares.ValidateJWT(controllers.SearchHandler)).Methods("POST")
 }
