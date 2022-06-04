bea66126c16c6324754c245a68e631e7e4c99b6e
Marck Munoz <marck527@gmail.com>
2022-06-04 23:53:35 +0000

    .

--- backend/deployment/redeploy.sh
+++ backend/deployment/redeploy.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 # ensure we are in the correct branch
+echo "Checking out working-on-ubuntu-branch.."
 git checkout working-on-ubuntu
 
 # pull latest
+echo "Pulling latest.."
 git pull
 
 # restart appgo service
+echo "Restarting the appgo service.."
 sudo service appgo restart 
+
+echo "Redeploy completed."
